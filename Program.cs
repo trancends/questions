d49b814617b6496108686a52d4a189dc30ecf43c
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-05-21 15:38:20 +0700

    [ADD] soal2d

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ class Program
         Console.WriteLine();
         Soal2c(5);
         Console.WriteLine();
+        Soal2d(5);
+        Console.WriteLine();
     }
 
     static void Soal1(int n)
@@ -97,6 +99,53 @@ class Program
                 }
             }
 
+            lists.Add(list);
+            Console.WriteLine();
+        }
+    }
+
+    static void Soal2d(int n)
+    {
+        var lists = new List<List<int>>();
+        for (int i = 1; i <= n; i++)
+        {
+            var list = new List<int>();
+            // Console.WriteLine("i: " + i);
+            // Console.WriteLine("Count: " + lists.Count);
+
+            for (int j = 1; j <= n; j++)
+            {
+                if (i == 1)
+                {
+                    if (j == 1)
+                    {
+                        list.Add(j);
+                    }
+                    else
+                    {
+                        if (list[^1] % 2 == 0)
+                        {
+                            list.Add(list[^1] + 1);
+                        }
+                        else
+                        {
+                            list.Add(list[^1] + 9);
+                        }
+                    }
+                }
+                else
+                {
+                    if (lists[0][j - 1] % 2 == 0)
+                    {
+                        list.Add(lists[^1][j - 1] - 1);
+                    }
+                    else
+                    {
+                        list.Add(lists[^1][j - 1] + 1);
+                    }
+                }
+                Console.Write(list[j - 1] + " ");
+            }
             lists.Add(list);
             Console.WriteLine();
         }
