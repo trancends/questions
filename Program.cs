45a9a6f1727efcfde2bb30ac618b44b474294e04
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-05-21 13:33:43 +0700

    [ADD] soal1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BankIndonesia;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Soal1(15);
+    }
+
+    static void Soal1(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("OK ");
+            }
+            else if (i % 4 == 0)
+            {
+                Console.Write("YES ");
+            }
+            else if (i % 3 == 0 && i % 4 == 0)
+            {
+                Console.Write("OKYES ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
